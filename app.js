9b279e081f11a41298e34bb9ac82f601f150335a
Yulia Jaworski <yuliashusharinav@yandex.ru>
2023-09-22 14:50:18 +0500

    fix: install cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const { error } = require('./middlwares/errors');
 const { errorLogger, requestLogger } = require('./middlwares/logger');
@@ -10,6 +11,13 @@ const { PORT, dataMovies } = require('./config');
 const router = require('./routes');
 
 const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3001', 'https://movies-explorer-jj.nomoredomainsicu.ru/'],
+  }),
+);
 
 mongoose.connect(dataMovies, {
   useNewUrlParser: true,
